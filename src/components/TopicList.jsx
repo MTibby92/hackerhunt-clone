f3a3880251995cd3bb7f4a427bead36e509f6d64
Matt Tibby <matthew.tibby.contractor@macmillan.com>
2017-12-18 17:54:57 -0600

    rework styles and how topics are rendered

--- src/components/TopicList.jsx
+++ src/components/TopicList.jsx
@@ -2,23 +2,95 @@ import React from "react";
 import Topic from "./Topic";
 import icon from "../icons-001@2x.png";
 
-const TopicList = () => (
-    <aside id="topicsContainer">
-        <h3>Topics</h3>
-        <ul>
-            <Topic href="/" name="Development" rule="-247px 0px" />
-            <Topic href="/" name="System" rule="-577px 0px" />
-            <Topic href="/" name="Tools" rule="-594px 0px" />
-            <Topic href="/" name="Data science" rule="-429px 0px" />
-            <Topic href="/" name="Blockchain" rule="-379px 0px" />
-            <Topic href="/" name="Mobile" rule="-478px 0px" />
-            <Topic href="/" name="Awesome lists" rule="-462px 0px" />
-            <Topic href="/" name="Social" rule="-363px 0px" />
-            <Topic href="/" name="Visual" rule="0px 0px" />
-            <Topic href="/" name="Open source" rule="-165px 0px" />
-            <Topic href="/" name="All topics" rule="-313px 0px" />
-        </ul>
-    </aside>
-);
+const TopicList = () => {
+    const topics = [
+        {
+            href: "/topic/development/trending",
+            name: "Development",
+            rule: "-247px 0px",
+            subTopics: [
+                {
+                    href: "/topic/devops/trending",
+                    name: "Devops",
+                    rule: "-445px 0px",
+                },
+                {
+                    href: "/topic/database/trending",
+                    name: "Databases",
+                    rule: "-231px 0px",
+                },
+                {
+                    href: "/topic/api/trending",
+                    name: "Apis",
+                    rule: "-198px 0px",
+                },
+                {
+                    href: "/topic/library/trending",
+                    name: "Libraries",
+                    rule: "-66px 0px",
+                },
+            ],
+        },
+        {
+            href: "/",
+            name: "System",
+            rule: "-577px 0px",
+        },
+        {
+            href: "/",
+            name: "Tools",
+            rule: "-594px 0px",
+        },
+        {
+            href: "/",
+            name: "Data science",
+            rule: "-429px 0px",
+        },
+        {
+            href: "/",
+            name: "Blockchain",
+            rule: "-379px 0px",
+        },
+        {
+            href: "/",
+            name: "Mobile",
+            rule: "-478px 0px",
+        },
+        {
+            href: "/",
+            name: "Awesome lists",
+            rule: "-462px 0px",
+        },
+        {
+            href: "/",
+            name: "Social",
+            rule: "-363px 0px",
+        },
+        {
+            href: "/",
+            name: "Visual",
+            rule: "0px 0px",
+        },
+        {
+            href: "/",
+            name: "Open source",
+            rule: "-165px 0px",
+        },
+        {
+            href: "/",
+            name: "All topics",
+            rule: "-313px 0px",
+        },
+    ];
+    return (
+        <aside id="topicsContainer">
+            <h3>Topics</h3>
+            <ul id="topicsList">
+                {topics.map( ( topic, i ) =>
+                    <Topic key={ i } href={ topic.href } name={ topic.name } rule={ topic.rule } /> )}
+            </ul>
+        </aside>
+    );
+}
 
 export default TopicList;
