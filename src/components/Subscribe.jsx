98daae2e3948f35ac03800efb05d479dc441ba1c
Matt Tibby <matthew.tibby.contractor@macmillan.com>
2018-01-10 13:12:25 -0600

    add skeleton for subscribe component

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const Subscribe = () => (
     <div id="subscribeContainer">
-        <p>Subscribe container here</p>
+        <h1>Get high on categorized<br />Show HN submissions</h1>
+        <p>We know the struggle. A lot of great stuff on Hacker News gets lost in the /shownew limbo.</p>
+        <p>Get the weekly top Show HN picks in your inbox!</p>
+        <form>
+            <input type="email" placeholder="Your e-mail" value="" />
+            <input type="submit" value="Subscribe" />
+        </form>
+        <p>Prouldly brought to you by <a href="/" style={ { color: "#f60" } }>Finch.io</a></p>
     </div>
 );
 
